8aeb4b01d893ed0833386d001a9e8963d0965447
Vahid <vahidmenu@gmail.com>
2023-03-13 16:43:32 +0100

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ff587d084e9e7a7"; }
+/******/ 		__webpack_require__.h = function() { return "a6e2ccb58e9b78f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
