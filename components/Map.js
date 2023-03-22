e07ad1ba7c6a466dac7dce53082a810083d52030
Vahid <vahidmenu@gmail.com>
2023-03-22 03:58:27 +0100

    map fix changes

--- components/Map.js
+++ components/Map.js
@@ -1,27 +1,34 @@
 import { useRef, useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, {
+    Marker,
+    Popup,
+    NavigationControl,
+    FullscreenControl
+  } from "react-map-gl";
  
 
 
 const Map = () => {
-//     const mapRef = useRef(null)
     const [viewport, setViewport] = useState({
         width:"100%",
-        height:"100%",
-         latitude: 46, 
-         longitude: -79, 
-         zoom: 10 })
+        height:"90%",
+        latitude: 46, 
+        longitude: -79, 
+        zoom: 8 })
 
 
     return (
-
+        
         <ReactMapGL
             mapStyle="mapbox://styles/vahidnesro/clfj29gzs000801o9jsa0ik4c"
             mapboxAccessToken={process.env.mapbox_key}
             {...viewport}
-            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            onViewportChange={(viewport) => {setViewport(viewport)}}
             // ref={(instance) => (mapRef.current = instance)}
+            
             ></ReactMapGL>
+        
+        
     )
 }
 
